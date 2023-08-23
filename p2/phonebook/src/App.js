744e91c68f35816885602b2fd0a0200fb9aefa03
untalmike <miguel.gonzalezl@hotmail.com>
2023-08-22 23:02:34 -0600

    2.9: Phonebook step 4

--- p2/phonebook/src/App.js
+++ p2/phonebook/src/App.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
+  const [ finder, setFinder ] = useState('')
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
 
@@ -14,6 +18,13 @@ const App = () => {
   const handleNumber = (e) => {
     setNewNumber(e.target.value)
   }
+
+  const changeFinder = (e) => {
+    setFinder(e.target.value)
+  }
+
+  const generalInfo = persons.map( person => person )
+  const filterInfo = generalInfo.filter(display => (display.name.toLowerCase().includes(`${finder}`)))
 
   const addName = (e) => {
     e.preventDefault()
@@ -34,9 +45,14 @@ const App = () => {
     
   }
 
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Filter shown with: <input type='search' value={finder} onChange={changeFinder}/></div>
+
+      <h2>Add a new</h2>
       <form onSubmit={addName}>
         <div>name: <input onChange={handleName} value={newName}/></div>
         <div>number: <input onChange={handleNumber} value={newNumber}/></div>
@@ -44,14 +60,16 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-        {persons.map((person, i) => {
-          const {name, number} = person
-            return(
-              <p key={i}>{name} {number}</p>
-              )
-            })
-          }
+        {
+          finder === ""
+          ?
+            generalInfo.map(general => <p key={general.name}>{general.name} {general.number}</p>)
+          :
+            filterInfo.map(info => <p key={info.name}>{info.name} {info.number}</p>)
+        }
+
     </div>
   )
 }
